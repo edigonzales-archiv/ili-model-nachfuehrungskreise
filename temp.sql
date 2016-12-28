8eaf4e1898b5f486867e946f526896380e585112
Stefan Ziegler <edi.gonzales@gmail.com>
2016-12-28 17:06:08 +0100

    Initialimport

---
+++ temp.sql
@@ -0,0 +1,7 @@
+﻿INSERT INTO av_nachfuehrungskreise_3.nachfuehrngsgmter_firmenstandort 
+       (aname, zusatzname, uid, lokalisationsname, hausnummer, plz, ortschaft,
+        telefonnummer, faxnummer, e_mail, web)
+
+SELECT aname, zusatzname, uid, lokalisationsname, hausnummer, plz, ortschaft,
+       telefonnummer, faxnummer, e_mail, web
+FROM av_nachfuehrungskreise_2.nachfuehrngsgmter_firma
---
+++ temp2.sql
@@ -0,0 +1,8 @@
+﻿SELECT *
+FROM av_nachfuehrungskreise_3.nachfuehrngskrise_nachfuehrungskreis as k,
+     av_nachfuehrungskreise_3.nachfuehrngsgmter_kreis_geometer as kg,
+     av_nachfuehrungskreise_3.nachfuehrngsgmter_nachfuehrungsgeometer as nf,
+     av_nachfuehrungskreise_3.nachfuehrngsgmter_firmenstandort as f
+WHERE k.t_id = kg.kreis
+AND nf.t_id = kg.geometer
+AND nf.t_id = f.geometer
